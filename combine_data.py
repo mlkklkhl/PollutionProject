e39cd11169eb2238d13fc791c2c1929c33aad023
Mallika Kliangkhlao <42213474+mlkklkhl@users.noreply.github.com>
2024-09-01 17:21:10 +0700

    Update combine_data.py

--- combine_data.py
+++ combine_data.py
@@ -24,7 +24,6 @@ def combine_weather():
 
     return combined_data
 
-
 def combine_pm_air4thai():
     combined_data = pd.DataFrame()
 
@@ -35,7 +34,7 @@ def combine_pm_air4thai():
         print(f"Reading {csv_file}...")
         df = pd.read_csv(csv_file)
 
-        if csv_file != 'PM2.5/prep_data\PM2.5(2024).csv':
+        if csv_file != 'PM2.5/prep_data\\PM2.5(2024).csv':
             # if the column 44T exists then rename it to PM2.5A
             if '44T ' in df.columns:
                 df = df.rename(columns={'44T ': '44T'})
@@ -46,8 +45,10 @@ def combine_pm_air4thai():
             # drop rows with missing values in 44T column
             df = df.dropna(subset=['PM2.5A'])
 
-            # change 00:00 to 01:00
             df['DateTime'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S')
+
+            # check if DateTime is 00:00:00 then change to 01:00:00
+            df.loc[df['DateTime'].dt.hour == 0, 'DateTime'] = df['DateTime'] + pd.DateOffset(hours=1)
             df = df[['DateTime', 'PM2.5A']]
 
         else:
@@ -55,6 +56,7 @@ def combine_pm_air4thai():
             df['DateTime'] = df.iloc[:, 1] + ' ' + df.iloc[:, 2]
 
             df['DateTime'] = pd.to_datetime(df['DateTime'], format='%Y-%m-%d %H:%M:%S')
+
             df = df.rename(columns={'pm2.5': 'PM2.5A'})
             df = df[['DateTime', 'PM2.5A']]
 
@@ -76,6 +78,9 @@ if __name__ == "__main__":
     weather_data = combine_weather()
     pmA_data = combine_pm_air4thai()
 
+    weather_data.reset_index(inplace=True)
+    pmA_data.reset_index(inplace=True)
+
     # print data description
     print("Weather data description:")
     print(weather_data.describe().to_string(), '\n')
@@ -83,7 +88,18 @@ if __name__ == "__main__":
     print("PM2.5A data description:")
     print(pmA_data.describe().to_string(), '\n')
 
-    combined_data = pd.merge(weather_data, pmA_data, left_index=True, right_index=True, how='left')
+    # concat the weather and PM2.5A data
+    combined_data = pd.merge(weather_data, pmA_data, on='DateTime', how='outer')
+
+    # sort the DataFrame by the DateTime index
+    combined_data.set_index('DateTime', inplace=True)
+    combined_data.sort_values(by='DateTime', inplace=True)
+
+    # print combined data description
+    print("Combined data description:")
+    print(combined_data.describe().to_string(), '\n')
+
+    # Save the combined data to a single CSV file
     combined_data.to_csv('combined_data.csv')
 
     print(f"Finish saving combined data to CSV file combined_data.csv")
